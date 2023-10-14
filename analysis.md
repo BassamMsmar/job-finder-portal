9fa579652bfd42bdf0e7d6aead1aab41d7121730
bassammsmar <bassammsmar@gmail.com>
2023-10-14 14:43:04 +0300

    create model to jobs and company

---
+++ analysis.md
@@ -0,0 +1,24 @@
+- services 
+
+jobs: 
+- title 
+- servise
+- hours
+- location 
+- salary_range
+- job_description
+- required_skills
+- education_experience
+- create_date
+- application_date
+- number_of_vacancies
+- job_nature 
+
+- company :
+    - company_name
+    - company_logo
+    - company_website
+    - company_email
+    - company_description
+
+
---
+++ jobs/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+
+JOB_NATURE_CHOICES = (
+        ('Full Time', 'Full-Time'),
+        ('Part Time', 'Part-Time'),
+        ('Remote', 'Remote'),
+        ('Freelance', 'Freelance'),
+    )
+
+
+SERVICE_CHOICES = (
+        ('Design & Creative', 'Design & Creative'),
+        ('Design & Development', 'Design & Development'),
+        ('Sales & Marketing', 'Sales & Marketing'),
+        ('Mobile Application', 'Mobile Application'),
+        ('Construction', 'Construction'),
+        ('Information Technology', 'Information Technology'),
+        ('Real Estate', 'Real Estate'),
+        ('Content Writer', 'Content Writer'),
+    )
+
+# Create your models here.
+class Jobs(models.Model):
+    title = models.CharField(max_length=255)
+    service = models.CharField(max_length=50, choices=SERVICE_CHOICES)
+    company = models.ForeignKey('Company', related_name='jobs_company', on_delete=models.CASCADE)
+    hours = models.IntegerField()
+    location = models.CharField(max_length=255)
+    salary_range = models.CharField(max_length=50)
+    job_description = models.TextField(max_length=2500)
+    required_skills = models.TextField(max_length=2500)
+    education = models.TextField(max_length=2500)
+    experience = models.IntegerField()
+    create_date = models.DateTimeField()
+    application_date = models.DateField()
+    number_of_vacancies = models.PositiveIntegerField()
+    job_nature = models.CharField(choices=JOB_NATURE_CHOICES, max_length=50)
+
+
+    def __str__(self):
+        return self.title
+    
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='company_logos/', null=True, blank=True)
+    website = models.URLField(max_length=200)
+    email = models.EmailField(max_length=100)
+    description = models.TextField(max_length=2500)
+
+    def __str__(self):
+        return self.name
